6aee00639837f6cadd7ae8384dc0c7b4bd676c38
paccyo <zaq1122s@gmail.com>
2025-05-12 12:01:17 +0900

    aa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,12 +11,12 @@ import { getFirestore, type Firestore } from 'firebase/firestore';
 // NEXT_PUBLIC_GOOGLE_MAPS_API_KEY="YOUR_GOOGLE_MAPS_API_KEY"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCnjI1sLDgAztoY26IdRMPqATARoD9qHuM",
+  authDomain: "chronotrack-u10nt.firebaseapp.com",
+  projectId: "chronotrack-u10nt",
+  storageBucket: "chronotrack-u10nt.firebasestorage.app",
+  messagingSenderId: "125029841927",
+  appId: "1:125029841927:web:97a624246dba73d9437ea2",
 };
 
 let app: FirebaseApp;
